2dd48da26f5d3b192329a8f85c611cfa455f06b0
peturaxel <pa14@ru.is>
2016-10-15 22:32:05 +0000

    test multiplenumbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -21,4 +21,11 @@ public class CalculatorTest {
 		assertEquals(3, Calculator.add("1,2"));
 	}	
 
+	@Test
+    public void testMultipleNumbers(){
+    	assertEquals(6, Calculator.add("1,2,3"));
+    }
+
+
+
 }
